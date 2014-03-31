1e1f8f9a3f07770f2362dc6212843b8feb1b2c3d
onlyzanna <onlyzanna@hotmail.it>
2014-04-01 00:52:54 +0200

    Initial Commit

---
+++ OpenGL_001_triangolo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_001_triangolo
+//
+//  Created by Davide Zanin on 01/04/14.
+//  Copyright (c) 2014 Davide Zanin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
